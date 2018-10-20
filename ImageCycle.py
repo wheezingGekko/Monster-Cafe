1860bb9c84b7437a8b21ec046d6dd224ba49e715
Farrah Shiela Urmeneta <farrah.s.urmeneta@hotmail.ca>
2018-10-20 11:46:37 -0600

    Added detection of image collision, ignoring possible transparency

--- ImageCycle.py
+++ ImageCycle.py
@@ -20,15 +20,125 @@ import math
 import random
 import enum
 
-class AnimatedImage(Image):
+class OverlappingImage(Image):
+    
+    '''
+    checks if the image is within another image horizontally
+    '''
+    def _in_widget_horizontal(self, widget):
+        return (widget.x <= self.x < widget.right or widget.x <= self.right < widget.right)
+
+    '''
+    checks if the image is within the other image horizontally
+    '''
+    def _in_widget_vertical(self, widget):
+        return (widget.y <= self.y < widget.top or widget.y <= self.top < widget.top)
+
+    '''
+    calculates a range based on the desired axis (x or y)
+    '''
+    def _calculate_range(self, widget, axis, mini, maxa):
+        self_hv = getattr(self, axis)
+        self_core_hv = getattr(self._coreimage, axis)
+        widget_hv = getattr(widget, axis)
+        widget_core_hv = getattr(self._coreimage, axis)
+
+        self_min = getattr(self, mini)
+        self_max = getattr(self, maxa)
+        widget_min = getattr(widget, mini)
+        widget_max = getattr(widget, maxa)
+
+        axis_padding = (self_hv - self_core_hv)/2
+        axis_range = [math.ceil(widget_core_hv/2)]
+        start = -1
+        step = 0
+
+        if self_min < widget_min:
+            start = self_max - widget_min
+            end = -1
+            step = -1
+        elif self_min > widget_min:
+            start = 0
+            end = widget_max - self_min
+            step = 1
+
+        # if the image isn't exactly on top of the other vertically
+        # or horizontally, then we remove the padding
+        if start > -1:
+            if start > 0:
+                start -= axis_padding
+            if end > -1:
+                end -= axis_padding
+            axis_range = range(math.ceil(start), math.ceil(end), step)
+
+        return axis_range, step
+
+    '''
+    note: what if overlapping over multiple things?
+    '''
+    def is_overlapping(self, widget):
+
+        if (self._in_widget_horizontal(widget) and self._in_widget_vertical(widget)):
+
+            # reading the _coreimage is essentially the image itself on the file, which doesn't
+            # regard the stretches we did on top of the image
+            # that is why we have to consider the padding when we search for the actual "pixel" of the image
+            
+            if (self.x == widget.x and self.y == widget.y):
+                return True
+
+            h_range, h_step = self._calculate_range(widget, 'width', 'x', 'right')
+            v_range, v_step = self._calculate_range(widget, 'height', 'y', 'top')
+             
+            for h in h_range:
+                self_h_pixel, widget_h_pixel = h, h
+
+                 # coming from the right, the widget's desired pixel is on the right
+                if h_step == 1:
+                    widget_h_pixel = widget._coreimage.width - h
+                # coming from the left, the instance's desired pixel is on the right
+                elif h_step == -1:
+                    self_h_pixel = self._coreimage.width - h
+
+                if (widget_h_pixel >= widget._coreimage.width 
+                        or widget_h_pixel < 0 
+                        or self_h_pixel >= self._coreimage.width 
+                        or self_h_pixel < 0):
+                    continue
+
+                for v in v_range:
+                    self_v_pixel, widget_v_pixel = v, v
+
+                    # coming from above
+                    if v_step == 1:
+                        widget_v_pixel = widget._coreimage.height - v
+                    # coming from below
+                    elif v_step == -1:
+                        self_v_pixel = self._coreimage.height - v
+                    
+                    if (widget_v_pixel >= widget._coreimage.height 
+                            or widget_v_pixel < 0 
+                            or self_v_pixel >= self._coreimage.height 
+                            or self_v_pixel < 0):
+                        continue
+
+                    if (self._coreimage.read_pixel(self_h_pixel, self_v_pixel)[-1] != 0 
+                            and widget._coreimage.read_pixel(widget_h_pixel, widget_v_pixel)[-1] != 0):
+                        return True
+        return False
+
+class AnimatedImage(OverlappingImage):
+    '''
+    Class handling images that are manually animated, either with looping
+    images or with x- y- axis movement.
+    '''
+
     MOVEMENT_SPEED = 5
     CYCLE_LENGTH = 4
 
     image_name = StringProperty('')
     image_state = StringProperty('')
     image_num = NumericProperty(0)
-
-    orientation = 0
 
     # image stretched because of how tiny the sprites are
     def __init__(self, **kwargs):
@@ -37,24 +147,18 @@ class AnimatedImage(Image):
         self.width = 150
         self.size_hint_y = None
         self.allow_stretch = True
+        self.keep_ratio = True
+        self.keep_data = True
 
-    def _idle(self):
+    def idle(self):
         self.image_state = "idle"
-
-    '''
-        TODO: This doesn't work, so it's not inside
-    '''
-    def flip(self):
-        self.orientation = (self.orientation + 1) % 2
-        self.texture.flip_horizontal()
 
     def move(self,  offset): 
         self.image_state = "run"
         self.pos = (self.MOVEMENT_SPEED * Vector(offset)) + self.pos 
 
-    def update(self):
+    def updateImage(self):
         self.image_num = (self.image_num + 1) % self.CYCLE_LENGTH
-
 
 class WalkingCharacter(AnimatedImage):
 
@@ -63,49 +167,45 @@ class WalkingCharacter(AnimatedImage):
 
     def setPatrol(self, movementList):
         self.MOVEMENT_LIST = movementList
-        self.currentGoal = 1
-        self.currentMovement = [0, 0]
+        self._current_goal = 1
+        self._current_movement = [0, 0]
 
-    def updateCurrentMovement(self):
-        self.currentMovement[0] = self.MOVEMENT_LIST[self.currentGoal][0]
-        self.currentMovement[1] = self.MOVEMENT_LIST[self.currentGoal][1]
+    def updateMovement(self):
+        if (self._current_movement[0] == 0 and self._current_movement[1] == 0):
+            self._current_movement[0] = self.MOVEMENT_LIST[self._current_goal][0]
+            self._current_movement[1] = self.MOVEMENT_LIST[self._current_goal][1]
+            self._current_goal = (self._current_goal + 1) % len(self.MOVEMENT_LIST)
+             
+        self._moveTowardGoal()
 
-    def update(self):
-        super(WalkingCharacter, self).update()
-        if (self.currentMovement[0] == 0 and self.currentMovement[1] == 0):
-            self.updateCurrentMovement()
-            self.currentGoal = (self.currentGoal + 1) % len(self.MOVEMENT_LIST) 
-            print (self.currentMovement)
-        self.moveTowardGoal()
+    def _moveTowardGoal(self):
+        move_vector = Vector(0, 0)
 
-    def moveTowardGoal(self):
-        moveVector = Vector(0, 0)
-
-        xMove = 0
-        yMove = 0
+        x_move, y_move = 0, 0
 
         # if the expected movement is leftward (-x), move to the left
-        if self.currentMovement[0] < 0:
-            xMove = -1
+        if self._current_movement[0] < 0:
+            x_move = -1
         # if the expected movement is rightward (+x), move to the right
-        elif self.currentMovement[0] > 0:
-            xMove = 1
+        elif self._current_movement[0] > 0:
+            x_move = 1
 
         # if the expected movement is downward (-y), move down
-        if self.currentMovement[1] < 0:
-            yMove = -1
+        if self._current_movement[1] < 0:
+            y_move = -1
         # if the expected movement is upward (+y), move up
-        elif self.currentMovement[1] > 0:
-            yMove = 1
+        elif self._current_movement[1] > 0:
+            y_move = 1
 
         # we subtract from the amount of movement we have left
-        self.currentMovement[0] -= xMove
-        self.currentMovement[1] -= yMove
+        self._current_movement[0] -= x_move
+        self._current_movement[1] -= y_move
 
         # then we make the expected movement
-        moveVector += Vector(xMove, yMove)
+        move_vector += Vector(x_move, y_move)
 
-        self.move(moveVector)
+        self.move(move_vector)
+
 
 # trying to overlay counters on to floor
 class Counter(FloatLayout):
@@ -140,7 +240,6 @@ class ImageCycleExample(Widget):
     monsterImage = ObjectProperty(None)
     otherMonsterImage = ObjectProperty(None)
     storeFloor = ObjectProperty(None)
-    storeCounter = ObjectProperty(None)
 
     OTHER_MONSTER_MOVEMENT = [[50, 0], [-50, 0]]
 
@@ -153,15 +252,12 @@ class ImageCycleExample(Widget):
             pass
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self._keyboard.bind(on_key_up=self._on_keyboard_up)
-        #self.monsterImage.bind(texture=self._on_update_texture)
 
     def _keyboard_closed(self):
-        print('Keyboard has closed')
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-
         # TODO// Create a function that checks if the movement is allowed using vectors
         if keycode[1] == "down" and (self.monsterImage.y - self.monsterImage.MOVEMENT_SPEED) >= 0:
             self.monsterImage.move((0,-1))
@@ -176,7 +272,7 @@ class ImageCycleExample(Widget):
     def _on_keyboard_up(self, keyboard, keycode):
         if (keycode[1] == "down" or keycode[1] == "up" or
             keycode[1] == "left" or keycode[1] == "right"):
-                self.monsterImage._idle()       
+                self.monsterImage.idle()       
 
     #def _on_update_texture(self, instance, value):
         #self.monsterImage.texture = value
@@ -184,19 +280,26 @@ class ImageCycleExample(Widget):
     def build(self):
         self.storeFloor.build()
         self.otherMonsterImage.setPatrol(self.OTHER_MONSTER_MOVEMENT)
-        self.storeCounter.build()
 
-    def update(self, dt):
-        self.monsterImage.update()
-        self.otherMonsterImage.update()
+    def updateImage(self, dt):
+        self.monsterImage.updateImage()
+        self.otherMonsterImage.updateImage()
+    
+    def updateMovement(self, dt):
+        if not self.monsterImage.is_overlapping(self.otherMonsterImage):
+            self.otherMonsterImage.updateMovement()
 
 
 class ImageCycleApp(App):
 
+    IMAGE_UPDATE_SPEED = 10.0/60.0
+    MOVEMENT_UPDATE_SPEED = 5.0/60.0
+
     def build(self):
         example = ImageCycleExample()
         example.build()
-        Clock.schedule_interval(example.update, 10.0/60.0)
+        Clock.schedule_interval(example.updateImage, self.IMAGE_UPDATE_SPEED)
+        Clock.schedule_interval(example.updateMovement, self.MOVEMENT_UPDATE_SPEED)
         return example
 
 if __name__ == '__main__':
